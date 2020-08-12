223f7d409d95011a181bd4f5266cfea8ab943d8a
Oliver Switzer <oliverswitzer@gmail.com>
2020-08-12 11:16:57 -0400

    Add tests, an a simple maze test case

--- test/board.exs
+++ test/board.exs
@@ -2,7 +2,30 @@ defmodule BoardTest do
   use ExUnit.Case
   doctest Board
 
-  test "creates grid of cells" do
+  setup do
+    maze = """
+    ***
+    *Es
+    ***
+    """ |> String.trim
 
+    File.write("test/sample_maze.txt", maze)
+
+    on_exit fn ->
+      if File.exists?("test/sample_maze.txt"), do: File.rm("test/sample_maze.txt")
+    end
+  end
+
+  test "creates grid of cells with the correct start and exit" do
+    result = Board.new("test/sample_maze.txt")
+
+    number_of_cells = Enum.count(Map.keys(result))
+    assert number_of_cells == 9
+
+    start_coord = result[%Coordinate{x: 2, y: 1}]
+    assert start_coord == %Cell{type: :start, occupied_by: :player}
+
+    end_coord = result[%Coordinate{x: 1, y: 1}]
+    assert end_coord == %Cell{type: :exit, occupied_by: nil}
   end
 end
